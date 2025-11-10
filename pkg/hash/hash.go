69147fe71679f470a5cf79967ee58fcbba115508
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-11-10 16:32:47 -0300

    feat(hash): import hash package

---
+++ pkg/hash/hash.go
@@ -0,0 +1,63 @@
+// Copyright (C) 2025 Alan Barbosa Lima.
+//
+// Almodon is licensed under the GNU General Public License
+// version 3. You should have received a copy of the
+// license, located in LICENSE, at the root of the source
+// tree. If not, see <https://www.gnu.org/licenses/>.
+
+// Package hash wraps functionality from the standard
+// [golang.org/x/crypto/bcrypt] package.
+package hash
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// Takes an arbitrarily long password and hashes it using the BCrypt
+// algorithm.
+//
+// To compare a hash to its password, you MUST use the [Compare]
+// function, hashing the password again and using == should yield a
+// different hash, failing the equality test.
+//
+// It handles the passwords that are larger than 72 bytes, which
+// BCrypt wouldn't accept, by XORing the content over itself in
+// 72-byte chuncks. This introduces chance for matching a passwords
+// in simplitic manner, because XOR is trivially reversible.
+func Hash(password []byte) ([60]byte, error) {
+	ingest := atMax72Bytes(password)
+
+	digest, err := bcrypt.GenerateFromPassword(ingest, bcrypt.DefaultCost)
+	if err != nil {
+		return [60]byte{}, err
+	}
+	if len(digest) != 60 {
+		return [60]byte{}, errors.New("hash: digest is not of length 60")
+	}
+
+	return [60]byte(digest), nil
+}
+
+// Compares a hash generated through [Hash] with a password, it
+// returns true for a match, and false for not a match or an error.
+func Compare(hash, password []byte) bool {
+	err := bcrypt.CompareHashAndPassword(hash, atMax72Bytes(password))
+	return err == nil
+}
+
+func atMax72Bytes(data []byte) []byte {
+	const size = 72
+
+	if len(data) <= size {
+		return data
+	}
+
+	result := make([]byte, size)
+	for i, d := range data {
+		result[i%size] ^= d
+	}
+
+	return result
+}
---
+++ pkg/hash/hash_test.go
@@ -0,0 +1,33 @@
+package hash_test
+
+import (
+	crand "crypto/rand"
+	"math/rand/v2"
+	"testing"
+
+	. "github.com/alan-b-lima/almodon/pkg/hash"
+)
+
+func TestHashComparison(t *testing.T) {
+	const set_size = 100
+
+	const max_len = 100
+	const min_len = 8
+
+	buf := make([]byte, 0, max_len)
+
+	for range set_size {
+		password := buf[:rand.IntN(max_len-min_len)+min_len]
+		crand.Read(password)
+
+		hash, err := Hash(password)
+		if err != nil {
+			t.Errorf("following error shouldn't have happened: %v", err)
+			continue
+		}
+
+		if !Compare(hash[:], password) {
+			t.Errorf("%x should have compared to true with its hash", password)
+		}
+	}
+}
