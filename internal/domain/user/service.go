cd240119c35c75a6c6b3784926fc65c3561b14ca
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-11-11 13:46:06 -0300

    feat(user): implement user functionalities

---
+++ internal/domain/user/service.go
@@ -0,0 +1,16 @@
+package user
+
+import (
+	"github.com/alan-b-lima/almodon/internal/auth"
+)
+
+type Service interface {
+	List(act auth.Actor, req ListRequest) (ListResponse, error)
+	Get(act auth.Actor, req GetRequest) (Response, error)
+	GetBySIAPE(act auth.Actor, req GetBySIAPERequest) (Response, error)
+	Create(act auth.Actor, req CreateRequest) (Response, error)
+	Patch(act auth.Actor, req PatchRequest) (Response, error)
+	Delete(act auth.Actor, req DeleteRequest) error
+	Authenticate(req AuthRequest) (AuthResponse, error)
+	Actor(req ActorRequest) (auth.Actor, error)
+}
--- internal/xerrors/errors.go
+++ internal/xerrors/errors.go
@@ -3,11 +3,26 @@ package xerrors
 import "github.com/alan-b-lima/almodon/pkg/errors"
 
 var (
+	ErrSessionTooLong = errors.Fmt(errors.InvalidInput, "session-too-long", "session must not last longer than %v")
+
 	ErrSessionNotFound = errors.New(errors.NotFound, "session-not-found", "session not found", nil)
-	ErrSessionTooLong  = errors.Fmt(errors.InvalidInput, "session-too-long", "session must not last longer than %v")
 )
 
 var (
+	ErrUserCreation = errors.Imp(errors.InvalidInput, "user-creation", "given data does not satisfy the user type")
+
+	ErrNameEmpty                     = errors.New(errors.InvalidInput, "name-empty", "name cannot be empty", nil)
+	ErrEmailInvalid                  = errors.New(errors.InvalidInput, "email-invalid", "email must be valid", nil)
+	ErrPasswordTooShort              = errors.New(errors.InvalidInput, "password-too-short", "password must be at least 8 characters long", nil)
+	ErrPasswordTooLong               = errors.New(errors.InvalidInput, "password-too-long", "password must be a maximum of 64 characters long", nil)
+	ErrPasswordLeadOrTrailWhitespace = errors.New(errors.InvalidInput, "password-edge-whitespace", "password must not begin or end with whitespaces", nil)
+	ErrPasswordIllegalCharacters     = errors.New(errors.InvalidInput, "password-illegal-chars", "password must not contain unprintable or invalid uft-8 characters", nil)
+
+	ErrIncorrectPassword    = errors.New(errors.Unauthorized, "incorrect-password", "given password is incorrect", nil)
+	ErrFailedToHashPassword = errors.Imp(errors.Internal, "hash-failure", "failed to hash the password")
+
 	ErrUnauthenticatedUser = errors.Imp(errors.Unauthorized, "unauthenticated-user", "user is not logged in")
-	ErrUnauthorizedUser    = errors.Fmt(errors.Forbidden, "unauthorized-user", "auth level %v does not match any criteria in %v")
+	ErrUnauthorizedUser    = errors.Fmt(errors.Forbidden, "unauthorized-user", "auth role %v does not match any criteria in %v")
+
+	ErrUserNotFound = errors.New(errors.NotFound, "user-not-found", "user not found", nil)
 )
