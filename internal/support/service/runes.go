9dabfb6279258a590840209cfce03cc7e7e40739
Alan Lima <alanlimanvc@gmail.com>
2026-05-24 14:39:38 -0300

    feat(service/runes): add string length validation functions

---
+++ internal/support/service/runes.go
@@ -0,0 +1,49 @@
+package service
+
+import "unicode/utf8"
+
+func StringLenMin(s string, min int) int {
+	if len(s) < min {
+		return -1
+	}
+
+	if utf8.RuneCountInString(s) < min {
+		return -1
+	}
+
+	return 0
+}
+
+func StringLenMax(s string, max int) int {
+	if len(s)/utf8.MaxRune > max {
+		return 1
+	}
+
+	if utf8.RuneCountInString(s) > max {
+		return 1
+	}
+
+	return 0
+}
+
+func StringLenBetween(s string, min, max int) int {
+	if len(s) < min {
+		return -1
+	}
+
+	if len(s)/utf8.MaxRune > max {
+		return 1
+	}
+
+	count := utf8.RuneCountInString(s)
+
+	if count < min {
+		return -1
+	}
+
+	if count > max {
+		return 1
+	}
+
+	return 0
+}
