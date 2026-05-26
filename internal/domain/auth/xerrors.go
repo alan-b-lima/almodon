a8181d101eeae9404ed4311904cdc0acb497fba3
Alan Lima <alanlimanvc@gmail.com>
2026-05-25 21:04:21 -0300

    feat(auth): add blocked error for completely locked resource

--- internal/domain/auth/xerrors.go
+++ internal/domain/auth/xerrors.go
@@ -6,5 +6,7 @@ import (
 
 var (
 	ErrUnauthenticated = problem.Imp(problem.Unauthenticated, "unauthenticated").Message("unauthenticated user")
-	ErrUnauthorized    = problem.Imp(problem.Unauthorized, "unauthorized").Format("actor role %v does not inherit any roles in %v")
+
+	ErrUnauthorized = problem.Imp(problem.Unauthorized, "unauthorized").Format("actor role %v does not inherit any roles in %v")
+	ErrBlocked      = problem.New(problem.Unauthorized, "blocked", "no actor is allowed", nil, nil)
 )
