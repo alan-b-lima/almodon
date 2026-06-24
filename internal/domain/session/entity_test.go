9fc9d890a9210aa01d1fd41849aeb86c9080a583
Alan Lima <alanlimanvc@gmail.com>
2026-06-23 22:10:07 -0300

    fix(tests): correct function name in token inversibility test

--- internal/domain/session/entity_test.go
+++ internal/domain/session/entity_test.go
@@ -6,14 +6,14 @@ import (
 	. "github.com/alan-b-lima/almodon/internal/domain/session"
 )
 
-func TestInversabilityBetweenStringAndFromString(t *testing.T) {
+func Test_InversabilityBetweenStringAndParseString(t *testing.T) {
 	const num_tests = 1000
 
 	for range num_tests {
 		token := NewToken()
 
 		str := token.String()
-		if back, err := FromString(str); err != nil {
+		if back, err := ParseString(str); err != nil {
 			t.Error(err)
 		} else if token != back {
 			t.Errorf("%x and %x should be equal", token, back)
